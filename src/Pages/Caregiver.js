3a9efc471e3ce003ffa8fa0eeee430b4079a6c82
Fengax <857514.leofeng@gmail.com>
2022-02-26 16:19:13 +1100

    added states

--- src/Pages/Caregiver.js
+++ src/Pages/Caregiver.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import { styled } from '@mui/material';
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
@@ -11,32 +11,15 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Main(){
     const [currentPage, setCurrentPage] = useContext(CurrentPageContext);
+    const [patientData, setPatientData] = useState([]);
+    const [detailedView, setDetailedView] = useState(false);
 
-    /* HELPER TO FIGURE OUT FORMATS
-    const Flashcard_button = styled(Button)(({ theme }) => ({
-        fontFamily: [
-            'Oxygen',
-            'Arial',
-            'sans-serif',
-            '"Apple Color Emoji"',
-            '"Segoe UI Emoji"',
-            '"Segoe UI Symbol"',
-        ].join(','),
-        color: '#000000',
-        backgroundColor: '#fffefc',
-        fontSize: '0.8vw',
-        height: 118,
-        width: '97%',
-        border: '2px solid #6ca1f0',
-        borderRadius: '16px',
-        marginTop: '0.25vw',
-        position: 'relative',
-        marginLeft: '5%',
-        textTransform: 'capitalize'
-    }));*/
+    useEffect(() => {
+        setPatientData([{name: "Bob", stage: 4, age: 52}, {name: "Bob2", stage: 4, age: 52}, {name: "Bob2", stage: 4, age: 52}, {name: "Bob2", stage: 4, age: 52}, {name: "Bob2", stage: 4, age: 52}, {name: "Bob2", stage: 4, age: 52}, {name: "Bob2", stage: 4, age: 52}, {name: "Bob2", stage: 4, age: 52}, {name: "Bob", stage: 4, age: 52}])
+    }, [])
 
-    
-    function Patient(props) {
+
+    function PatientCard() {
         const Info = styled(Button)(({ theme }) => ({
             color: '#000000',
             backgroundColor: "#BAD39F",
@@ -47,34 +30,74 @@ export default function Main(){
             minwidth: '20%', // TO CHANGE
             borderRadius: '12px',
             position: 'relative',
-            
+
             '&:hover': {
                 backgroundColor: "#A6BE8D",
                 borderColor: "#000000",
             }
-    
         }));
 
-
-        return (
-        <Box
-        sx={{
-          p: 1,
-          m: 1,
-          borderRadius: 2,
-          fontFamily: 'Nunito',
-        }}>
-        <Info 
-            icon={<AccountCircleIcon style={{color: '#ede7e3'}}/>}> 
-            <Typography>
-                {props.name} - {props.age} <br/>
-                Stage {props.stage}
-            </Typography>
-        </Info>
-
-        </Box>
-        );
+        return patientData.map((obj) => {
+                return (
+                    <Info>
+                        <div style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center"
+                        }}>
+                            <AccountCircleIcon style={{color: '#ede7e3', fontSize: 40, marginBottom: "1vh"}}/>
+                            <Typography>
+                                {obj.name} - {obj.age} <br/>
+                                Stage {obj.stage}
+                            </Typography>
+                        </div>
+                    </Info>
+                )
+            });
   }
+
+    function Overview(){
+        return (
+            <div>
+                <Typography sx={{
+                    fontFamily: 'Nunito',
+                    fontSize: 20,
+                    textAlign: "left",
+                    margin: '3vh',
+                }}
+                >
+                    Patients
+
+                </Typography>
+                <Box
+                    sx={{
+                        border: '2px solid #000000', // to remove
+                        margin: '5vh',
+                        marginLeft: '3vh',
+                        marginRight: '3vh',
+                        marginTop: '-2vh',
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        flexDirection: "row",
+                        columnGap: "4.6vw",
+                        rowGap: "3vh",
+                        justifyContent: 'left',
+                        alignContent: 'flex-start',
+                        p: 1,
+                        overflow: 'auto',
+                        m: 1,
+                        bgcolor: 'background.paper',
+                        borderRadius: 1,
+                    }}>
+
+                    <PatientCard onClick={() => {
+                        setCurrentPage("login")
+                    }}/>
+
+                </Box>
+            </div>
+        )
+    }
 
     return(
         <div>
@@ -84,85 +107,31 @@ export default function Main(){
                     fontFamily: 'Oxygen',
                     fontSize:'260%',
                     color: '#000000',
-                    textAlign: "left",  
+                    textAlign: "left",
                     marginLeft: '2vw',
-                    border: '1px solid #000000', 
+                    border: '1px solid #000000',
                 }}>
- 
+
                 Doctor Page
 
                 </Typography>
-            </div> 
-            <div>
-            <Box sx={{
-                border: '2px solid #6A874B',
-                borderRadius: '4px',
-                marginTop: '0.5%',
-                height: '70vh',
-                flexDirection: 'row',
-                width: '90.6vw',
-                overflow: 'auto',
-                marginLeft: '2vw', // adjust
-                backgroundColor: '#FFFCD6',
-            }}>
-                <Typography sx={{
-                    fontFamily: 'Nunito',
-                    fontSize: 20,
-                    textAlign: "left",
-                    margin: '3vh',
-                    }}
-                >
-                    Patients
-
-                </Typography>
-
-                
-
-                <Box
-                    sx={{
-                    border: '2px solid #000000', // to remove
-                    margin: '5vh',
-                    marginLeft: '3vh',
-                    marginRight: '3vh',
-                    marginTop: '-2vh',
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    justifyContent: 'space-evenly',
-                    alignContent: 'flex-start',
-                    p: 1,
-                    overflow: 'auto',
-                    m: 1,
-                    bgcolor: 'background.paper',
-                    borderRadius: 1,
-                    }}>
-
-                    <Patient onClick={() => {
-                        setCurrentPage("login")
-                    }} name = "Bob" age ='87' stage = '3'/> 
-                    
-                </Box>
-            </Box>
             </div>
-
+            <div>
+                <Box sx={{
+                    border: '2px solid #6A874B',
+                    borderRadius: '4px',
+                    marginTop: '0.5%',
+                    height: '70vh',
+                    flexDirection: 'row',
+                    width: '90.6vw',
+                    overflow: 'auto',
+                    marginLeft: '2vw', // adjust
+                    backgroundColor: '#FFFCD6',
+                }}>
+                    {detailedView === false && Overview()}
+                </Box>
+            </div>
         </div>
 
     )
 }
-/* <NormalButton onClick={() => {
-                setCurrentPage("login")
-            }}>Login page</NormalButton> 
-            
-            
-<Box sx = {{
-                    border: '2px solid #6A874B',
-                    margin: '5vh',
-                    marginTop: '-2vh',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    flexWrap: 'wrap',
-                    justifyContent: 'space-evenly',
-                    alignContent: 'flex-start',
-                    overflow: 'auto',
-                    height: '65vh',
-                }}>
-                */
