78ebc9465c69ce643ae293b1b759614b866dbac3
blackberryfound <86906130+blackberryfound@users.noreply.github.com>
2022-02-27 06:49:43 +1100

    Text field layout changes

--- src/Components/AddPatientPopup.js
+++ src/Components/AddPatientPopup.js
@@ -6,6 +6,7 @@ import Modal from '@mui/material/Modal';
 import { TextField } from '@mui/material';
 import NormalButton from './NormalButton';
 import { typography } from '@mui/system';
+import {styled} from "@mui/material";
 
 export default function AddPatient(props){
     const nameTextRef = React.useRef('');
@@ -15,6 +16,31 @@ export default function AddPatient(props){
     const [usernameerror, setUsernameerror] = React.useState(false);
     const [ageerror, setAgeerror] = React.useState(false);
 
+
+    const InputField = styled(TextField)({
+        '& label.Mui-focused': {
+        color: '#6A874B',
+        },
+        '& .MuiInput-underline:after': {
+        borderBottomColor: '#6A874B',
+        },
+        '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: '#8AA861',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#6A874B',
+        },
+        },
+        '& .MuiInputBase-input': {
+            fontFamily: ['Comic Sans', '"Nunito"','"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',].join(','),
+        }
+    });
+  
     return(
         <Modal
             open={props.openpop}
@@ -22,17 +48,30 @@ export default function AddPatient(props){
             aria-labelledby="modal-addpatient"
             aria-describedby="modal-addpatient-content"
             sx = {{
-                width: '90%',
-                height: 650,
+                width: '88%',
+                height: 480,
                 position: 'absolute',
-                ml: '5%',
-                mt: '7%'
+                ml: '6%',
+                mt: '9%'
             }}
         >
-            <Box sx={{bgcolor: '#BAD39F', height: '100%', borderRadius: '4px'}} >
+            <Box sx={{bgcolor: '#BAD39F', height: '100%', borderRadius: '12px'}} >
+                
                 <Box>
-                    <Box>
-                    <TextField
+                    <Box sx={{border : '1px solid #BAD39F', borderRadius: '12px'}}>
+                    <Typography sx={{
+                        fontFamily: 'Nunito',
+                        fontSize: 35,
+                        textAlign: "left",
+                        marginTop: '5vh',
+                        marginLeft: '3vw'
+                        }}>
+
+                        Register a patient
+                    </Typography>
+                    </Box>
+                    <Box >
+                    <InputField
                         required
                         error={nameerror} 
                         id="Name-field" 
@@ -46,11 +85,12 @@ export default function AddPatient(props){
                             width: 1/2,
                             position: 'relative',
                             ml: '3vw',
-                            mt: '1vw'
+                            mt: '1vw',
+                            colour: '8AA861',
                     }}/>
                     </Box>
                     <Box>
-                    <TextField
+                    <InputField
                         required
                         error={usernameerror}  
                         id="Username-field" 
@@ -64,16 +104,20 @@ export default function AddPatient(props){
                             width: 1/2,
                             position: 'relative',
                             ml: '3vw',
-                            mt: '3vw'
+                            mt: '2vw'
                     }}/>
                     </Box>
                     <Box>
-                    <TextField
+                    <InputField
                         required
                         error={ageerror}  
                         id="Age-field" 
                         label="Age" 
                         variant="outlined"
+                        type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
                         inputRef={ageTextRef}
                         helperText="Enter the patient's age, only number is accepted"
                         sx={{
@@ -82,11 +126,11 @@ export default function AddPatient(props){
                             width: 1/4,
                             position: 'relative',
                             ml: '3vw',
-                            mt: '3vw'
+                            mt: '2vw'
                     }}/>
                     </Box>
                 </Box>
-                <Box sx={{position: 'relative', mt: '11vw', ml: '45%'}}>
+                <Box sx={{position: 'relative', mt: '2vw', ml: '45%'}}>
                     <NormalButton onClick={()=> {
                         setNameerror(false);
                         setUsernameerror(false);
