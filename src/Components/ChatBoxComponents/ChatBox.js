bf130cd1cbbee2168184500782397b7860f1123c
Mango23333 <75919015+Mango23333@users.noreply.github.com>
2022-02-27 11:41:58 +1100

    chat prototype

--- src/Components/ChatBoxComponents/ChatBox.js
+++ src/Components/ChatBoxComponents/ChatBox.js
@@ -1,7 +1,37 @@
+import React, {useEffect} from 'react';
 import { Box } from '@mui/system';
 import Messages from './MessagesComponents/Messages';
+import { TextField } from '@mui/material';
 
 const ChatBox = () => {
+    var textSendRef = React.useRef("");
+    const [defaultTextValue, setDefaultTextValue] = React.useState("")
+    var chatHistory = [];
+    var currentChat = {
+        texts: "",
+    //patient have number 1 and bot have number 0
+        sender: 0,
+    };
+
+    useEffect(() => {
+        const listener = event => {
+          if ((event.code === "Enter" || event.code === "NumpadEnter") && textSendRef.current.value.trim() != "") {
+            event.preventDefault();
+            currentChat.texts = textSendRef.current.value;
+            currentChat.sender = 1;
+            let currentChattemp = Object.assign({}, currentChat);
+            chatHistory.push(currentChattemp);
+            textSendRef.current.value = "";
+            console.log(chatHistory);
+            console.log("Enter key was pressed. Run your function.");
+          }
+        };
+        document.addEventListener("keydown", listener);
+        return () => {
+          document.removeEventListener("keydown", listener);
+        };
+      }, []);
+
   return (
     <div>
         <Box sx={{
@@ -14,19 +44,24 @@ const ChatBox = () => {
             overflow: 'auto',
             backgroundColor: '#FFFCD6'
         }}>
-            <Messages />
-            <Box sx={{
+            <Messages chathis={chatHistory} />
+        </Box>
+        <Box sx={{
                 height: "10vh",
-                padding: "3vh 0vh 3vh 0vh"
             }}>
-                <input  type="text"
-                        placeholder="Enter text here"
-                        style={{
-                            width: "80vw",
-                            height: "4vh",
-                            fontSize: "3vh",
-                        }} />
-            </Box>
+                <TextField 
+                    type="text"
+                    placeholder="Enter text here and press enter to send message"
+                    inputRef={textSendRef}
+                    value={textSendRef.current.value}
+                    style={{
+                        marginTop: '0.5vw',
+                        borderRadius: '4px',
+                        width: "90vw",
+                        fontSize: "3vh",
+                        backgroundColor: '#FDFAE2'
+                    }}
+                />
         </Box>
     </div>
   )
